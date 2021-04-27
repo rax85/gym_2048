d0d6b29bb15315e3955a151584d7bd9d5889035b
Rakesh Iyer <iyer.rakesh@gmail.com>
2021-04-26 22:51:56 -0700

    More requirements stuff.

--- setup.py
+++ setup.py
@@ -2,5 +2,10 @@ from setuptools import setup
 
 setup(name='gym_2048',
       version = '0.0.1',
-      install_requires = ['gym', 'numpy', 'pillow']
+      install_requires = [
+            'absl-py',
+            'gym',
+            'numpy',
+            'pillow'
+      ]
 )
